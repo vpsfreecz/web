13a94abb0254c8ce1ea62eceeb9c0c8d867c61c2
freedomlives <andrew@freedomlives.net>
2016-11-10 17:46:22 +0100

    Update spolecne.php

--- en/registration/spolecne.php
+++ en/registration/spolecne.php
@@ -75,7 +75,7 @@ if (!$db)
 			));
 		?>
 	</div>
-<p>If you need an invoice, please send billing information to our <a href="/contact/">support</a>.</p>
+<p>If you need an invoice, please send billing information to our <a href="/contact/">support team</a>.</p>
 </div>
 
 
