c8c0e318ddd8984ad44309d9482e26ce981ea01f
Jakub Skokan <jakub.skokan@havefun.cz>
2020-01-12 19:07:21 +0100

    Registration form: add platform info, per-platform distributions

--- cs/prihlaska/spolecne.php
+++ cs/prihlaska/spolecne.php
@@ -3,6 +3,28 @@ require_once dirname(__FILE__).'/../../lib/init.php';
 
 if (!$api)
 	$api = new \HaveAPI\Client(API_URL);
+
+$templates = array();
+$locations = array();
+
+foreach ($api->os_template->list() as $tpl) {
+	$templates[ $tpl->id ] = array(
+		'id' => $tpl->id,
+		'label' => $tpl->label,
+		'hypervisor_type' => $tpl->hypervisor_type,
+	);
+}
+
+$locList = $api->location->list(array(
+	'environment' => ENVIRONMENT_ID,
+	'has_hypervisor' => true,
+));
+
+foreach ($locList as $loc) {
+	$locations[$loc->id] = array(
+		'label' => $loc->label,
+	);
+}
 ?>
 
 <label for="name">Doplňující údaje</label>
@@ -20,36 +42,52 @@ if (!$api)
 
 </div>
 
-<label for="name">Nastavení VPS</label>
+<label for="location">Umístění VPS</label>
 <div class="row">
 	<div class="col-xs-12 form-group">
-		<?php
+			<?php
 			$opts = array();
 
-			foreach ($api->os_template->list() as $tpl) {
-				$opts[ $tpl->id ] = $tpl->label;
+			foreach ($locList as $loc) {
+				$opts[ $loc->id ] = 'Master Internet '.$loc->label;
 			}
 
-			$f->select('distribution', $opts);
+			$f->select('location', $opts);
 		?>
 	</div>
 
 </div>
 
+<label>Virtualizační platforma</label>
+<div class="row">
+	<noscript>Prosím zapni si JavaScript</noscript>
+	<p id="platform-choose" class="hidden">Vyber umístění VPS</p>
+	<p id="platform-vpsadminos" class="hidden">
+		<strong>vpsAdminOS</strong> je naším spolkem vyvinutý systém pro provoz
+		linuxových kontejnerů, které pohání naše VPS. Více informací viz
+		<a href="https://kb.vpsfree.cz/navody/vps/vpsadminos" target="_blank">znalostní báze</a>
+		nebo dokumentace projektu na
+		<a href="https://vpsadminos.org" target="_blank">vpsadminos.org</a>.
+	</p>
+	<p id="platform-openvz" class="hidden">
+		<strong>OpenVZ Legacy</strong> je dosluhující kontejnerová virtualizace.
+		V současné době přecházíme na nové řešení v podobě <strong>vpsAdminOS</strong>,
+		které je však zatím k dispozici pouze v Praze. Více informací viz
+		<a href="https://kb.vpsfree.cz/navody/vps/vpsadminos" target="_blank">znalostní báze</a>.
+	</p>
+</div>
+
+<label>Distribuce</label>
 <div class="row">
 	<div class="col-xs-12 form-group">
-			<?php
+		<?php
 			$opts = array();
-			$locations = $api->location->list(array(
-				'environment' => ENVIRONMENT_ID,
-				'has_hypervisor' => true,
-			));
 
-			foreach ($locations as $loc) {
-				$opts[ $loc->id ] = 'Master Internet '.$loc->label;
+			foreach ($templates as $id => $tpl) {
+				$opts[ $id ] = '['.$tpl['hypervisor_type'].'] '.$tpl['label'];
 			}
 
-			$f->select('location', $opts);
+			$f->select('distribution', $opts);
 		?>
 	</div>
 
@@ -77,3 +115,52 @@ if ($f->isResubmit()) {
 <div class="row">
 	<input class="btn btn-default" type="submit" id="send" value="Odeslat">
 </div>
+
+<script type="text/javascript">
+var locations = <?php echo json_encode($locations); ?>;
+var templates = <?php echo json_encode($templates); ?>;
+
+$('#platform-choose').removeClass('hidden');
+$('#distribution option:not([disabled])').remove();
+
+function changeLocation() {
+	var locId = $('#location').val().toString();
+	var loc = locations[locId];
+	var platform;
+
+	$('#platform-choose').addClass('hidden');
+
+	if (loc.label == 'Praha') {
+		platform = 'vpsadminos';
+		$('#platform-openvz').addClass('hidden')
+		$('#platform-vpsadminos').removeClass('hidden')
+	} else {
+		platform = 'openvz';
+		$('#platform-openvz').removeClass('hidden')
+		$('#platform-vpsadminos').addClass('hidden')
+	}
+
+	$('#distribution option:not([disabled])').remove();
+	$('#distribution option[disabled]').prop('selected', true);
+
+	for (const k of Object.keys(templates)) {
+		var tpl = templates[k];
+
+		if (tpl.hypervisor_type != platform)
+			continue;
+
+		$('#distribution').append($('<option>', {value: tpl.id, text: tpl.label}));
+	}
+}
+
+$('#location').change(changeLocation);
+
+<?php
+if ($f->isPost()) {
+	echo "changeLocation();\n";
+
+	if ($_POST['distribution'])
+		echo "$('#distribution').find('option[value=\"".$_POST['distribution']."\"]').prop('selected', true);\n";
+}
+?>
+</script>
--- en/registration/spolecne.php
+++ en/registration/spolecne.php
@@ -3,6 +3,28 @@ require_once dirname(__FILE__).'/../../lib/init.php';
 
 if (!$api)
 	$api = new \HaveAPI\Client(API_URL);
+
+$templates = array();
+$locations = array();
+
+foreach ($api->os_template->list() as $tpl) {
+	$templates[ $tpl->id ] = array(
+		'id' => $tpl->id,
+		'label' => $tpl->label,
+		'hypervisor_type' => $tpl->hypervisor_type,
+	);
+}
+
+$locList = $api->location->list(array(
+	'environment' => ENVIRONMENT_ID,
+	'has_hypervisor' => true,
+));
+
+foreach ($locList as $loc) {
+	$locations[$loc->id] = array(
+		'label' => $loc->label,
+	);
+}
 ?>
 
 <label for="name">Additional info</label>
@@ -20,36 +42,53 @@ if (!$api)
 
 </div>
 
-<label for="name">VPS settings</label>
+<label for="location">VPS location</label>
 <div class="row">
 	<div class="col-xs-12 form-group">
-		<?php
+			<?php
 			$opts = array();
 
-			foreach ($api->os_template->list() as $tpl) {
-				$opts[ $tpl->id ] = $tpl->label;
+			foreach ($locList as $loc) {
+				$opts[ $loc->id ] = 'Master Internet '.$loc->label;
 			}
 
-			$f->select('distribution', $opts);
+			$f->select('location', $opts);
 		?>
 	</div>
 
 </div>
 
+<label>Virtualization platform</label>
+<div class="row">
+	<noscript>Please enable JavaScript</noscript>
+	<p id="platform-choose" class="hidden">Choose VPS location</p>
+	<p id="platform-vpsadminos" class="hidden">
+		<strong>vpsAdminOS</strong> is an in-house developed container-based
+		virtualization platform that powers our VPS. For more information, see our
+		<a href="https://kb.vpsfree.org/manuals/vps/vpsadminos" target="_blank">knowledge base</a>
+		or project documentation at
+		<a href="https://vpsadminos.org" target="_blank">vpsadminos.org</a>.
+	</p>
+	<p id="platform-openvz" class="hidden">
+		<strong>OpenVZ Legacy</strong> is a container-based virtualization platform.
+		We are in the process of migrating to a more up-to-date solution called
+		<strong>vpsAdminOS</strong>, which is at this time available only in Prague
+		(Praha). For more information, see our
+		<a href="https://kb.vpsfree.org/manuals/vps/vpsadminos" target="_blank">knowledge base</a>.
+	</p>
+</div>
+
+<label>Distribution</label>
 <div class="row">
 	<div class="col-xs-12 form-group">
 		<?php
 			$opts = array();
-			$locations = $api->location->list(array(
-				'environment' => ENVIRONMENT_ID,
-				'has_hypervisor' => true,
-			));
 
-			foreach ($locations as $loc) {
-				$opts[ $loc->id ] = 'Master Internet '.$loc->label;
+			foreach ($templates as $id => $tpl) {
+				$opts[ $id ] = '['.$tpl['hypervisor_type'].'] '.$tpl['label'];
 			}
 
-			$f->select('location', $opts);
+			$f->select('distribution', $opts);
 		?>
 	</div>
 
@@ -77,3 +116,52 @@ if ($f->isResubmit()) {
 <div class="row">
 	<input class="btn btn-default" type="submit" id="send" value="Send">
 </div>
+
+<script type="text/javascript">
+var locations = <?php echo json_encode($locations); ?>;
+var templates = <?php echo json_encode($templates); ?>;
+
+$('#platform-choose').removeClass('hidden');
+$('#distribution option:not([disabled])').remove();
+
+function changeLocation() {
+	var locId = $('#location').val().toString();
+	var loc = locations[locId];
+	var platform;
+
+	$('#platform-choose').addClass('hidden');
+
+	if (loc.label == 'Praha') {
+		platform = 'vpsadminos';
+		$('#platform-openvz').addClass('hidden')
+		$('#platform-vpsadminos').removeClass('hidden')
+	} else {
+		platform = 'openvz';
+		$('#platform-openvz').removeClass('hidden')
+		$('#platform-vpsadminos').addClass('hidden')
+	}
+
+	$('#distribution option:not([disabled])').remove();
+	$('#distribution option[disabled]').prop('selected', true);
+
+	for (const k of Object.keys(templates)) {
+		var tpl = templates[k];
+
+		if (tpl.hypervisor_type != platform)
+			continue;
+
+		$('#distribution').append($('<option>', {value: tpl.id, text: tpl.label}));
+	}
+}
+
+$('#location').change(changeLocation);
+
+<?php
+if ($f->isPost()) {
+	echo "changeLocation();\n";
+
+	if ($_POST['distribution'])
+		echo "$('#distribution').find('option[value=\"".$_POST['distribution']."\"]').prop('selected', true);\n";
+}
+?>
+</script>
