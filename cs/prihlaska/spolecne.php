c6f87cb73687e61992a2eb3bcc75ae925bcd0eef
Jakub Skokan <jakub.skokan@havefun.cz>
2021-09-20 12:45:29 +0200

    Registration form: remove per-location virtualization platforms

--- cs/prihlaska/spolecne.php
+++ cs/prihlaska/spolecne.php
@@ -5,27 +5,18 @@ if (!$api)
 	$api = new \HaveAPI\Client(API_URL);
 
 $templates = array();
-$locations = array();
 
 foreach ($api->os_template->list() as $tpl) {
-	$templates[] = array(
-		'id' => $tpl->id,
-		'label' => $tpl->label,
-		'hypervisor_type' => $tpl->hypervisor_type,
-	);
+	if ($tpl->hypervisor_type != 'vpsadmin')
+		continue;
+
+	$templates[] = $tpl;
 }
 
-$locList = $api->location->list(array(
+$locations = $api->location->list(array(
 	'environment' => ENVIRONMENT_ID,
 	'has_hypervisor' => true,
 ));
-
-foreach ($locList as $loc) {
-	$locations[] = array(
-		'id' => $loc->id,
-		'label' => $loc->label,
-	);
-}
 ?>
 
 <label for="name">Doplňující údaje</label>
@@ -49,7 +40,7 @@ foreach ($locList as $loc) {
 			<?php
 			$opts = array();
 
-			foreach ($locList as $loc) {
+			foreach ($locations as $loc) {
 				$opts[ $loc->id ] = 'Master Internet '.$loc->label;
 			}
 
@@ -61,34 +52,12 @@ foreach ($locList as $loc) {
 
 <label>Virtualizační platforma</label>
 <div class="row">
-	<noscript>Prosím zapni si JavaScript</noscript>
-	<div id="platform-choose" class="col-xs-12 hidden">
-		<p>
-			V současné době používáme dvě virtualizační technologie. V Praze postupně
-			rozšiřujeme nasazení <strong>vpsAdminOS</strong>, což je námi vyvinutý systém
-			pro kontejnerovou virtualizaci. Více informací viz
-			<a href="https://kb.vpsfree.cz/navody/vps/vpsadminos" target="_blank">znalostní báze</a>
-			nebo dokumentace projektu na
-			<a href="https://vpsadminos.org" target="_blank">vpsadminos.org</a>.
-		</p>
-		<p>
-			V Brně je k prozatím dispozici jen <strong>OpenVZ Legacy</strong>. Jedná
-			se také o kontejnerovou virtualizaci, ale používá se zde starší Linux
-			kernel a postupně od ní upouštíme ve prospěch <strong>vpsAdminOS</strong>.
-		</p>
-	</div>
 	<p id="platform-vpsadminos" class="hidden">
 		<strong>vpsAdminOS</strong> je naším spolkem vyvinutý systém pro provoz
 		linuxových kontejnerů, které pohání naše VPS. Více informací viz
 		<a href="https://kb.vpsfree.cz/navody/vps/vpsadminos" target="_blank">znalostní báze</a>
 		nebo dokumentace projektu na
 		<a href="https://vpsadminos.org" target="_blank">vpsadminos.org</a>.
-	</p>
-	<p id="platform-openvz" class="hidden">
-		<strong>OpenVZ Legacy</strong> je dosluhující kontejnerová virtualizace.
-		V současné době přecházíme na nové řešení v podobě <strong>vpsAdminOS</strong>,
-		které je však zatím k dispozici pouze v Praze. Více informací viz
-		<a href="https://kb.vpsfree.cz/navody/vps/vpsadminos" target="_blank">znalostní báze</a>.
 	</p>
 </div>
 
@@ -99,7 +68,7 @@ foreach ($locList as $loc) {
 			$opts = array();
 
 			foreach ($templates as $tpl) {
-				$opts[ $tpl['id'] ] = '['.$tpl['hypervisor_type'].'] '.$tpl['label'];
+				$opts[ $tpl->id ] = $tpl->label;
 			}
 
 			$f->select('distribution', $opts);
@@ -134,53 +103,3 @@ if ($f->isResubmit()) {
 <div class="row">
 	<input class="btn btn-default" type="submit" id="send" value="Odeslat">
 </div>
-
-<script type="text/javascript">
-var locations = <?php echo json_encode($locations); ?>;
-var templates = <?php echo json_encode($templates); ?>;
-var locationIndex = {};
-
-locations.forEach(function(loc) {
-	locationIndex[loc.id] = loc;
-});
-
-$('#platform-choose').removeClass('hidden');
-$('#distribution option:not([disabled])').remove();
-
-function changeLocation() {
-	var locId = $('#location').val().toString();
-	var loc = locationIndex[locId];
-	var platform;
-
-	$('#platform-choose').addClass('hidden');
-
-	if (loc.label == 'Praha') {
-		platform = 'vpsadminos';
-		$('#platform-openvz').addClass('hidden')
-		$('#platform-vpsadminos').removeClass('hidden')
-	} else {
-		platform = 'openvz';
-		$('#platform-openvz').removeClass('hidden')
-		$('#platform-vpsadminos').addClass('hidden')
-	}
-
-	$('#distribution option:not([disabled])').remove();
-	$('#distribution option[disabled]').prop('selected', true);
-
-	templates.forEach(function(tpl) {
-		if (tpl.hypervisor_type == platform)
-			$('#distribution').append($('<option>', {value: tpl.id, text: tpl.label}));
-	});
-}
-
-$('#location').change(changeLocation);
-
-<?php
-if ($f->isPost()) {
-	echo "changeLocation();\n";
-
-	if ($_POST['distribution'])
-		echo "$('#distribution').find('option[value=\"".$_POST['distribution']."\"]').prop('selected', true);\n";
-}
-?>
-</script>
--- en/registration/spolecne.php
+++ en/registration/spolecne.php
@@ -5,27 +5,18 @@ if (!$api)
 	$api = new \HaveAPI\Client(API_URL);
 
 $templates = array();
-$locations = array();
 
 foreach ($api->os_template->list() as $tpl) {
-	$templates[] = array(
-		'id' => $tpl->id,
-		'label' => $tpl->label,
-		'hypervisor_type' => $tpl->hypervisor_type,
-	);
+	if ($tpl->hypervisor_type != 'vpsadmin')
+		continue;
+
+	$templates[] = $tpl;
 }
 
-$locList = $api->location->list(array(
+$locations = $api->location->list(array(
 	'environment' => ENVIRONMENT_ID,
 	'has_hypervisor' => true,
 ));
-
-foreach ($locList as $loc) {
-	$locations[] = array(
-		'id' => $loc->id,
-		'label' => $loc->label,
-	);
-}
 ?>
 
 <label for="name">Additional info</label>
@@ -49,7 +40,7 @@ foreach ($locList as $loc) {
 			<?php
 			$opts = array();
 
-			foreach ($locList as $loc) {
+			foreach ($locations as $loc) {
 				$opts[ $loc->id ] = 'Master Internet '.$loc->label;
 			}
 
@@ -61,35 +52,12 @@ foreach ($locList as $loc) {
 
 <label>Virtualization platform</label>
 <div class="row">
-	<noscript>Please enable JavaScript</noscript>
-	<div id="platform-choose" class="col-xs-12 hidden">
-		<p>
-			We're using two virtualization platforms. In Prague (Praha), we've started
-			to deploy <strong>vpsAdminOS</strong>, which is an in-house developed
-			solution for container virtualization. For more information, see our
-			<a href="https://kb.vpsfree.org/manuals/vps/vpsadminos" target="_blank">knowledge base</a>
-			or project documentation at
-			<a href="https://vpsadminos.org" target="_blank">vpsadminos.org</a>.
-		</p>
-		<p>
-			In Brno, only <strong>OpenVZ Legacy</strong> is supported for now. It's
-			a container-based virtualization as well, but dated and with older Linux
-			kernel. We're in the process of migrating to the newer <strong>vpsAdminOS</strong>.
-		</p>
-	</div>
 	<p id="platform-vpsadminos" class="hidden">
 		<strong>vpsAdminOS</strong> is an in-house developed container-based
 		virtualization platform that powers our VPS. For more information, see our
 		<a href="https://kb.vpsfree.org/manuals/vps/vpsadminos" target="_blank">knowledge base</a>
 		or project documentation at
 		<a href="https://vpsadminos.org" target="_blank">vpsadminos.org</a>.
-	</p>
-	<p id="platform-openvz" class="hidden">
-		<strong>OpenVZ Legacy</strong> is a container-based virtualization platform.
-		We are in the process of migrating to a more up-to-date solution called
-		<strong>vpsAdminOS</strong>, which is at this time available only in Prague
-		(Praha). For more information, see our
-		<a href="https://kb.vpsfree.org/manuals/vps/vpsadminos" target="_blank">knowledge base</a>.
 	</p>
 </div>
 
@@ -100,7 +68,7 @@ foreach ($locList as $loc) {
 			$opts = array();
 
 			foreach ($templates as $tpl) {
-				$opts[ $tpl['id'] ] = '['.$tpl['hypervisor_type'].'] '.$tpl['label'];
+				$opts[ $tpl->id ] = $tpl->label;
 			}
 
 			$f->select('distribution', $opts);
@@ -133,53 +101,3 @@ if ($f->isResubmit()) {
 <div class="row">
 	<input class="btn btn-default" type="submit" id="send" value="Send">
 </div>
-
-<script type="text/javascript">
-var locations = <?php echo json_encode($locations); ?>;
-var templates = <?php echo json_encode($templates); ?>;
-var locationIndex = {};
-
-locations.forEach(function(loc) {
-	locationIndex[loc.id] = loc;
-});
-
-$('#platform-choose').removeClass('hidden');
-$('#distribution option:not([disabled])').remove();
-
-function changeLocation() {
-	var locId = $('#location').val().toString();
-	var loc = locationIndex[locId];
-	var platform;
-
-	$('#platform-choose').addClass('hidden');
-
-	if (loc.label == 'Praha') {
-		platform = 'vpsadminos';
-		$('#platform-openvz').addClass('hidden')
-		$('#platform-vpsadminos').removeClass('hidden')
-	} else {
-		platform = 'openvz';
-		$('#platform-openvz').removeClass('hidden')
-		$('#platform-vpsadminos').addClass('hidden')
-	}
-
-	$('#distribution option:not([disabled])').remove();
-	$('#distribution option[disabled]').prop('selected', true);
-
-	templates.forEach(function(tpl) {
-		if (tpl.hypervisor_type == platform)
-			$('#distribution').append($('<option>', {value: tpl.id, text: tpl.label}));
-	});
-}
-
-$('#location').change(changeLocation);
-
-<?php
-if ($f->isPost()) {
-	echo "changeLocation();\n";
-
-	if ($_POST['distribution'])
-		echo "$('#distribution').find('option[value=\"".$_POST['distribution']."\"]').prop('selected', true);\n";
-}
-?>
-</script>
