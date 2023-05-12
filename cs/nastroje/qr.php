788ba3b54882c706776425df6b91817e8eced09d
Martin Myška <martin@martinmyska.cz>
2023-05-12 11:42:28 +0200

    Fix xzBinary path + SK variable symbol

--- cs/nastroje/qr.php
+++ cs/nastroje/qr.php
@@ -4,7 +4,6 @@ require_once '../../vendor/autoload.php';
 use rikudou\SkQrPayment\QrPayment;
 use Rikudou\Iban\Iban\IBAN;
 
-
 $output_textate = date("Ymd");
 //$iban = $_GET['iban'];
 $amount = $_GET['amount'];
@@ -12,29 +11,30 @@ $vs = $_GET['vs'];
 
 if($_GET['country'] == "cz") 
 {
-	include 'phpqrcode/qrlib.php'; 
-	$text = "SPD*1.0*ACC:CZ0420100000002200041594*AM:".$amount."*CC:CZK*X-VS:".$vs."*MSG:QRPLATBA"; 
-	   
-	$ecc = 'L'; 
-	$fileixel_Size = 5; 
-	$frame_Size = 5; 
+        include 'phpqrcode/qrlib.php'; 
+        $text = "SPD*1.0*ACC:CZ0420100000002200041594*AM:".$amount."*CC:CZK*X-VS:".$vs."*MSG:QRPLATBA"; 
 
-	QRcode::png($text, false, $ecc, $fileixel_Size, $frame_size); 
+        $ecc = 'L'; 
+        $fileixel_Size = 5; 
+        $frame_Size = 5; 
+
+        QRcode::png($text, false, $ecc, $fileixel_Size, $frame_size); 
 }
 else if($_GET['country'] == "sk") 
 {
 
-	$iban = 'SK2083300000002601502873';//$_GET['iban'];
+        $iban = 'SK2083300000002601502873';//$_GET['iban'];
 
-	$payment = new QrPayment(new IBAN($iban));
-	$payment->setAmount(floatval($amount))
-			->setComment('QR platba SK')
-			->setCurrency('EUR')
-			->setVariableSymbol($vs);
+        $payment = new QrPayment(new IBAN($iban));
+        $payment->setAmount(floatval($amount))
+                        ->setComment('QR platba SK')
+                        ->setCurrency('EUR')
+                        ->setVariableSymbol($vs)
+                        ->setXzBinary('/run/current-system/sw/bin/xz');
 
-	$qrCode = $payment->getQrCode();
-	header('Content-Type: image/png');
-	echo $qrCode->getRawString();
+        $qrCode = $payment->getQrCode();
+        header('Content-Type: image/png');
+        echo $qrCode->getRawString();
 }
  
-?> 
+?>
